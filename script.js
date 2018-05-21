e176a0fc83f3d77907d6ebc7f7cc574152a3e181
JasGri <38950869+JasGri@users.noreply.github.com>
2018-05-21 12:29:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,53 @@ window.onclick = function(event) {
   }
 };
 
-document.getElementById("myButton2").onclick = function() {showSave()};
+// adresformulier
 
 function showSave() {
     document.getElementById("myButton2").innerHTML = "opslaan";
+}
+
+function show_adressen(adres) {
+	 var element = document.getElementById("adres_lijst");
+	 var my_button = document.createElement("button");
+	 for (i = 0; i < adres.length; i++) {
+		my_button.id = adres[i].naam;
+		my_button.innerHTML = adres[i].naam;
+		my_button.onclick = function() {bewerk_adres(adres, i)};
+		element.appendChild(my_button) ;
+	 }
+	
+}
+
+function zoek_adres(adres, zoeknaam) {
+	for (i = 0; i < adres.length; i++) {
+		if (zoeknaam == adres[i].naam;  {
+			bewerk_adres(adres, i);
+		}
+	}
+}
+
+function bewerk_adres(adres, adresnummer) {
+		document.getElementById("Fullname").value = adres[adresnummer].naam;
+		document.getElementById("Streetname").value = adres[adresnummer].straatnaam;
+		document.getElementById("Postcode").value = adres[adresnummer].Postcode;
+		document.getElementById("Plaatsnaam").value = adres[adresnummer].plaats;
+		document.getElementById("Email").value = adres[adresnummer].Email;
+}
+
+function my_onload_function() {
+	document.getElementById("myButton2").onclick = function() {showSave(2)};
+
+		var adres =[];
+		adres.push("test");
+		adres.push({ naam:"Jasper", straatnaam:"", Postcode:"",plaats:"",Email:""}) ;
+		adres.push({ naam:"Martin", straatnaam:"", Postcode:"",plaats:"",Email:""}) ;
+		adres.push({ naam:"Marleen",straatnaam:"", Postcode:"",plaats:"",Email:""}) ;
+
+		
+		bewerk_adres(adres, 2);
+		show_adressen(adres) ;
+		
 }
 
 
